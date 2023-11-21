215ddc01930b502e97148a4cdb8cefaf11df75c9
Dragana Trajkovic <dragana.trajkovic@lilly021.com>
2023-11-21 16:10:15 +0100

    final

--- acrobat/blocks/eventwrapper/eventwrapper.js
+++ acrobat/blocks/eventwrapper/eventwrapper.js
@@ -14,7 +14,7 @@ const PREVIEW_DIS = 'preview-displayed';
 const TRY_ANOTHER = 'try-another-file-start';
 // const UPSELL_DIS = 'upsell-displayed';
 const FADE = 'review fade-in';
-window.dc_hosted.disableChromeExtensionBanner = true;
+const DISABLE_EXTENSION_BANNER = window.dc_hosted.disableExtensionBanner === true;
 
 export default function init(element) {
   const wrapper = element;
@@ -73,14 +73,14 @@ export default function init(element) {
       // Browser Extension
       if (!localStorage.fricBrowExt) {
         let extName;
-        if (browserName === 'Chrome' && !window.modalDisplayed && window.dc_hosted.disableChromeExtensionBanner !== true) {
+        if (browserName === 'Chrome' && !window.modalDisplayed && !DISABLE_EXTENSION_BANNER) {
           window.modalDisplayed = true;
           extName = '#chromeext';
           extID = 'efaidnbmnnnibpcajpcglclefindmkaj';
           extInstalled(extID, extName, browserName);
         }
 
-        if (browserName === 'Microsoft Edge' && !window.modalDisplayed && window.dc_hosted.disableChromeExtensionBanner !== true) {
+        if (browserName === 'Microsoft Edge' && !window.modalDisplayed && !DISABLE_EXTENSION_BANNER) {
           window.modalDisplayed = true;
           extName = '#edgeext';
           extID = 'elhekieabhbkpmcefcoobjddigjcaadp';
